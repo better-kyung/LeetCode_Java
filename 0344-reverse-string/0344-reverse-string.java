44cc005648fe947a9c8910929ec37a4c699245bb
Gyeom <125348113+better-gyeom@users.noreply.github.com>
2024-04-15 18:50:38 +0900

    Time: 6 ms (8.35%), Space: 51.4 MB (5.02%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int len = s.length;
+
+        for (int i = 0; i < len / 2; i++) {
+            char temp = s[i];
+            s[i] = s[len - 1 - i];
+            s[len - 1 - i] = temp;
+        }
+
+        System.out.println(Arrays.toString(s));
+    }
+}
