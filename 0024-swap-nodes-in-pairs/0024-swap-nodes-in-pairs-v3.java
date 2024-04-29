7b82fee961dbd239e8ba522a0fa38a079687ceda
Gyeom <125348113+better-gyeom@users.noreply.github.com>
2024-04-29 14:26:15 +0900

    Time: 0 ms (100%), Space: 40.9 MB (75.31%) - LeetHub

---
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs-v3.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        //현재 노드와 다음 노드가 있으면 반복
+        if (head != null && head.next != null) {
+            //다음 노드 선언
+            ListNode p = head.next;
+            //다음다음 노드를 파라미터로 전달하고 스왑된 값을 리턴받음
+            head.next = swapPairs(head.next.next);
+            //다음다음 노드는 현재 노드로 지정
+            p.next = head;
+            return p;
+        }
+        return head;
+
+    }
+}
