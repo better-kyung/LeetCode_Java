99ec4639f7469bb37cc6e678103c014d5da6e1b7
Da Kyung <125348113+better-kyung@users.noreply.github.com>
2024-08-16 16:26:14 +0900

    Time: 0 ms (100%), Space: 42.4 MB (58.02%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists-refer.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists-refer.java
@@ -10,13 +10,11 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        //종료조건
-        if (list1 == null) return list2;
+                if (list1 == null) return list2;
         if (list2 == null) return list1;
 
-        //list2의 값이 더 크면
         if (list1.val < list2.val) {
-            list1.next = mergeTwoLists(list1.next, list2); 
+            list1.next = mergeTwoLists(list1.next, list2);
             return list1;
         } else {
             list2.next = mergeTwoLists(list1, list2.next);
