9870c45778363c76804fab5c18ca8e4523024969
Da Kyung <125348113+better-kyung@users.noreply.github.com>
2024-09-05 14:15:07 +0900

    Time: 3 ms (84.98%), Space: 49.8 MB (29.31%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands-refer.java
+++ 0200-number-of-islands/0200-number-of-islands-refer.java
@@ -1,28 +1,28 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        int cnt = 0;
+        int count = 0;
 
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[i].length; j++) {
                 if (grid[i][j] == '1') {
                     dfs(i, j, grid);
-                    cnt++;
+                    count++;
                 }
             }
-        }
-        return cnt;
+        }    
+        return count;
     }
 
     public void dfs(int i, int j, char[][] grid) {
-        //종료조건
         if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == '0') {
             return;
         }
         grid[i][j] = '0';
-        dfs(i, j + 1, grid);
-        dfs(i, j - 1, grid);
         dfs(i + 1, j, grid);
         dfs(i - 1, j, grid);
-
+        dfs(i, j + 1, grid);
+        dfs(i, j - 1, grid);
     }
+
+
 }
