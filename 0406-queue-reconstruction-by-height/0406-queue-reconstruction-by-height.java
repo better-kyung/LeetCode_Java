2e41e80379af923de587f155f37b7f6abd461d65
Da Kyung <125348113+better-kyung@users.noreply.github.com>
2024-09-11 18:07:14 +0900

    Time: 5 ms (98.4%), Space: 45.3 MB (80.02%) - LeetHub

---
+++ 0406-queue-reconstruction-by-height/0406-queue-reconstruction-by-height.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[][] reconstructQueue(int[][] people) {
+        Queue<int[]> pq = new PriorityQueue<>(
+            (a, b) -> a[0] != b[0] ? b[0] - a[0] : a[1] - b[1]
+        );
+
+        for (int[] person : people) {
+            pq.add(person);
+        }
+
+        List<int[]> result = new ArrayList<>();
+        while (!pq.isEmpty()) {
+            int[] person = pq.poll();
+            result.add(person[1], person);
+        }
+
+        return result.toArray(new int[people.length][2]);
+    }
+}
